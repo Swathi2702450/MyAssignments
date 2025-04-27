8f8de9bf2ae1a2e6c2934928bd1949cb40c6cab6
Swathi2702450 <swathisridharan27@gmail.com>
2025-04-27 13:01:44 +0530

    Add files via upload
    day1

---
+++ Datatypes.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=23;
+		System.out.println(a);
+        
+		float b=91.0f ;
+		System.out.println(b);
+		
+		boolean c=true;
+		System.out.println(c);
+		
+		char d='S';
+		System.out.println(d);
+		
+		String e="Swathi";
+		System.out.println(e);
+	}
+
+}
